98a47b80bd1828d930075e1843f1b5fe6fe9272c
MD. NASIM UDDIN <43935904+mdnasim1552@users.noreply.github.com>
2026-04-02 04:09:24 +0100

    Refactor object insertion and add dominance functions

--- Inventory/wwwroot/js/src/coronaryDiagram/index.js
+++ Inventory/wwwroot/js/src/coronaryDiagram/index.js
@@ -6,6 +6,7 @@ import * as insertObjectInsideLink from './insertObjectInsideLink.js';
 import * as splitLinkAndInertObject from './splitLinkAndInertObject.js';
 import * as restoreFromSnapshot from './restoreFromSnapshot.js';
 import * as addNoteToElement from './addNoteToElement.js';
+import * as dominance from './dominance.js';
 import './styles.css';
 const { TangentDirections } = joint.connectors.curve;
 const borderWidth = 4;
@@ -2094,7 +2095,7 @@ $("#insert-object").on("click", function () {
         return;
     }
 
-    insertObjectByVessel(vesselName, objectType);
+    insertObjectInsideLink.insertObjectByVessel(vesselName, objectType, graph, paper, joint);
 });
 function parseStenosis(graphJSON) {
 
@@ -2156,68 +2157,6 @@ function parseStenosis(graphJSON) {
 
     return results;
 }
-function insertObjectByVessel(vesselName, objectType) {
-
-    const links = graph.getLinks();
-
-    for (const link of links) {
-
-        const labels = link.get('labels');
-        if (!labels) continue;
-
-        for (const label of labels) {
-
-            const name = label.attrs?.labelText?.text;
-
-            if (name !== vesselName) continue;
-
-            // middle of label range
-            const min = label.range?.min ?? 0;
-            const max = label.range?.max ?? 1;
-
-            const ratio = (min + max) / 2;
-
-            const linkView = paper.findViewByModel(link);
-            if (!linkView) return;
-
-            const point = linkView.getPointAtRatio(ratio);
-
-
-            const x = point.x;
-            const y = point.y;
-            const exists = graph.getElements().some(el => {
-                const attachment = el.get('linkAttachment');
-
-                if (!attachment || !attachment.linkId || attachment.ratio == null) {
-                    return false;
-                }
-                //console.log(attachedLinkId);
-                //console.log(link.id);
-                //console.log(attachedRatio + " " + min + " " + max);
-                return (
-                    attachment.linkId === link.id &&
-                    attachment.ratio >= min &&
-                    attachment.ratio <= max
-                );
-            });
-
-            if (exists) {
-                alert("Object already exists in this vessel segment!");
-                return;
-            }
-            if (objectType === "Worm") {
-                insertObjectInsideLink.insertWormOnLink(link, ratio, "#000000", graph, paper, joint);
-            } else if (objectType === "UpBottomStroke") {
-                insertObjectInsideLink.insertUpBottomStroke(link, ratio, graph, paper, joint);
-            } else if (objectType === "Stent") {
-                insertObjectInsideLink.insertStentOnLink(link, ratio, "#000000", graph, paper, joint);
-            }
-            return;
-        }
-    }
-
-    console.warn("Vessel not found:", vesselName);
-}
 let cabgMode = false;
 let startPoint = null;
 let startAnchor = null;
@@ -2272,274 +2211,17 @@ $("#toggleControlsBtn").on("click", function () {
     }
 });
 $("#leftDomBtn").on("click", function () {
-    const mB2LinkModel = graph.getCell('mB2Link');
-    const mB2LinkChild1Model = graph.getCell('mB2LinkChild1');
-    const mB2LinkChild2Model = graph.getCell('mB2LinkChild2');
-    const mB2LinkChild3Model = graph.getCell('mB2LinkChild3');
-    const mB2LinkChild4Model = graph.getCell('mB2LinkChild4');
-    if (!mB2LinkModel) return;
-
-    // 3️⃣ Update the link
-    mB2LinkModel.set({
-        source: { x: 994, y: 1296 },
-        target: { x: 1491, y: 1294 },
-        vertices: [
-            { x: 1040, y: 1345 }, { x: 1109, y: 1390 }, { x: 1189, y: 1416 }, { x: 1260, y: 1428 }, { x: 1323, y: 1416 },
-            { x: 1400, y: 1371 }, { x: 1453, y: 1327 }
-        ],
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: 'CIRC AV' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ]
-    });
-    mB2LinkChild1Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .67 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: '1st LPL' } },
-                position: { distance: 0.7, angle: 10 }
-            }
-        ],
-    });
-    mB2LinkChild2Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .47 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: '2nd LPL' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ]
-    });
-    mB2LinkChild3Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .27 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: '3rd LPL' } },
-                position: { distance: 0.6, angle: 10 }
-            }
-        ]
-    });
-    mB2LinkChild4Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .03 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: 'IPDA' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ]
-    });
+    dominance.LeftDominance(graph);
     hideAllLinkLabels();
     $("#toggleLblBtn").text("Show Labels");
 });
 $("#rightDomBtn").on("click", function () {
-    const mB2LinkModel = graph.getCell('mB2Link');
-    const mB2LinkChild1Model = graph.getCell('mB2LinkChild1');
-    const mB2LinkChild2Model = graph.getCell('mB2LinkChild2');
-    const mB2LinkChild3Model = graph.getCell('mB2LinkChild3');
-    const mB2LinkChild4Model = graph.getCell('mB2LinkChild4');
-    if (!mB2LinkModel) return;
-
-    // 3️⃣ Update the link
-    mB2LinkModel.set({
-        source: { x: 827, y: 1431 },
-        target: { x: 1342, y: 1407 },
-        vertices: [
-            { x: 894, y: 1382 }, { x: 945, y: 1335 }, { x: 994, y: 1296 }, { x: 1040, y: 1345 }, { x: 1109, y: 1390 }, { x: 1189, y: 1416 }, { x: 1260, y: 1428 }
-        ],
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: 'rPAV' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ]
-    });
-    mB2LinkChild1Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .97 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: '1st RPL' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ],
-    });
-    mB2LinkChild2Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .8 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: '2nd RPL' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ]
-    });
-    mB2LinkChild3Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .63 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: '1st RPL' } },
-                position: { distance: 0.6, angle: 10 }
-            }
-        ]
-    });
-    mB2LinkChild4Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .36 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: 'rPDA' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ]
-    });
+    dominance.RightDominance(graph);
     hideAllLinkLabels();
     $("#toggleLblBtn").text("Show Labels");
 });
 $("#coDomBtn").on("click", function () {
-    const mB2LinkModel = graph.getCell('mB2Link');
-    const mB2LinkChild1Model = graph.getCell('mB2LinkChild1');
-    const mB2LinkChild2Model = graph.getCell('mB2LinkChild2');
-    const mB2LinkChild3Model = graph.getCell('mB2LinkChild3');
-    const mB2LinkChild4Model = graph.getCell('mB2LinkChild4');
-    if (!mB2LinkModel) return;
-
-    // 3️⃣ Update the link
-    mB2LinkModel.set({
-        source: { x: 827, y: 1431 },
-        target: { x: 1491, y: 1294 },
-        vertices: [
-            { x: 894, y: 1382 }, { x: 945, y: 1335 }, { x: 994, y: 1296 }, { x: 1040, y: 1345 }, { x: 1109, y: 1390 }, { x: 1189, y: 1416 }, { x: 1260, y: 1428 }, { x: 1323, y: 1416 },
-            { x: 1400, y: 1371 }, { x: 1453, y: 1327 }
-        ],
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: 'CIRC AV' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ]
-    });
-    mB2LinkChild1Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .75 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: '1st LPL' } },
-                position: { distance: 0.7, angle: 10 }
-            }
-        ],
-    });
-    mB2LinkChild2Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .6 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: '2nd LPL' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ]
-    });
-    mB2LinkChild3Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .48 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: '3rd LPL' } },
-                position: { distance: 0.6, angle: 10 }
-            }
-        ]
-    });
-    mB2LinkChild4Model.set({
-        source: {
-            id: mB2LinkModel.id,
-            anchor: {
-                name: 'connectionRatio',
-                args: { ratio: .28 } // 👈 new ratio here
-            }
-        },
-        labels: [
-            {
-                range: { min: 0, max: 1 },
-                attrs: { labelText: { text: 'IPDA' } },
-                position: { distance: 0.5, angle: 10 }
-            }
-        ]
-    });
+    dominance.CoDominance(graph);
     hideAllLinkLabels();
     $("#toggleLblBtn").text("Show Labels");
 });
